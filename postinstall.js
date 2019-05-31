524679094fbd18102f996e5c6fb6577c4ab81d34
DimitarTachev <nevermindd7@gmail.com>
2019-05-31 09:52:49 +0300

    fix: update to Babel 7

--- postinstall.js
+++ postinstall.js
@@ -1,11 +1,2 @@
 var hook = require('nativescript-hook')(__dirname);
 hook.postinstall();
-
-var projectDir = process.env['TNS_PROJECT_DIR'];
-require('child_process').exec('npm install --save-dev babel-core', { cwd: projectDir }, function (err, stdout, stderr) {
-	if (err) {
-		console.warn('npm: ' + err.toString());
-	}
-	process.stdout.write(stdout);
-	process.stderr.write(stderr);
-});
